840fa8b16af11d7267a972d4b70923134fb5ab77
plauer <plauer411@gmail.com>
2014-12-11 04:22:24 -0700

    completes socket setup in client's app.js

--- client/app.js
+++ client/app.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-  // Initialise Socket.io
+
+  // -------------------------
+  // INITIALIZE SOCKET CONNECTION
   var socket = io.connect('/');
 
-  socket.on('user:connect', function(user_count) {
-    update_user_count( user_count );
+
+  // --------------------------
+  //SOCKET.IO EVENTS
+  socket.on('user:connect', function(userCount) {
+    updateUserCount( userCount );
   });
 
-  socket.on('user:disconnect', function(user_count) {
-      update_user_count( user_count );
+  socket.on('user:disconnect', function(userCount) {
+      updateUserCount( userCount );
   });
+
+
+  // --------------------------
+  //UPDATED ACTIVE CONNECTION
+  var $userCount = $("#userCount")
+  var updateUserCount = function(count) {
+    $userCount.text( (count === 1) ? " You seem to be alone. Invite some friends!" : " " + count );
+  }
 
 });
